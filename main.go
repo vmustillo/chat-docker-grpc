31b736c8468c96f1714f429389d6fd580d41229e
Vincent Mustillo <vjmustillo@gmail.com>
2019-10-02 22:43:21 -0400

    Created a function to broadcast a message to one or many active connections using goroutines. Also created a simple grpc server listening to TCP traffic in the main function

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net"
 	"os"
+	"sync"
 
 	"chat-docker-grpc/proto"
 
+	"google.golang.org/grpc"
 	glog "google.golang.org/grpc/grpclog"
 )
 
@@ -42,6 +47,57 @@ func (s *Server) CreateStream(pconn *proto.Connect, stream proto.Broadcast_Creat
 	return <-conn.error
 }
 
-func main() {
+// BroadcastMessage - creates a goroutine for each connection on the server and sends the message in a stream to any active connection
+// if there is an error, the error is sent to the connection
+// Waits for all the goroutines to close before exiting
+func (s *Server) BroadcastMessage(ctx context.Context, msg *proto.Message) (*proto.Close, error) {
+	wait := sync.WaitGroup{}
+	done := make(chan int)
 
+	for _, conn := range s.Connection {
+		wait.Add(1)
+
+		go func(msg *proto.Message, conn *Connection) {
+			defer wait.Done()
+
+			if conn.active {
+				err := conn.stream.Send(msg)
+				grpcLog.Info("Sending message to: ", conn.stream)
+
+				if err != nil {
+					grpcLog.Errorf("Error with Stream: %s - Error: %v", conn.stream, err)
+					conn.active = false
+					conn.error <- err
+				}
+			}
+		}(msg, conn)
+	}
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+
+	<-done
+
+	return &proto.Close{}, nil
+
+}
+
+func main() {
+	var connections []*Connection
+
+	server := &Server{connections}
+
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatalf("Error creating the server %v", err)
+	}
+
+	grpcLog.Info("Starting server at port :8080")
+
+	proto.RegisterBroadcastServer(grpcServer, server)
+	grpcServer.Serve(listener)
 }
